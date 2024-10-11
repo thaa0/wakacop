f64d39214c3c973151059d26a13b6c35327c5026
Thalita Souza <dev.thalita@gmail.com>
2024-10-11 16:41:50 -0300

    init

---
+++ src/test/java/academy/wakanda/Wakacop/WakacopApplicationTests.java
@@ -0,0 +1,13 @@
+package academy.wakanda.Wakacop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WakacopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
